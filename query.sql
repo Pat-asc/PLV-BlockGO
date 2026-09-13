c9432927f3897ee532c3cfc9cf8d1a92407e4954
Cookies-Dookies <pascualpatrick264@gmail.com>
2026-09-13 17:41:39 +0800

    fixed: IAC

--- query.sql
+++
@@ -1,1 +0,0 @@
-﻿DELETE FROM password_reset_requests WHERE email = 'system-admin@plv.edu.ph';
--- query_clean.sql
+++
@@ -1,2 +0,0 @@
-﻿DELETE FROM studentprofiles WHERE student_no = '2026-99999';
-DELETE FROM users WHERE email = 'jdoe99999@plv.edu.ph';
